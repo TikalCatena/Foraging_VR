fd1ba4c0de2dc52a325c64676eb8097a08564bff
Mitch Munns <mitchmunns@ucsb.edu>
2022-08-23 22:04:55 -0700

    location tracker works
    saves camera position as x, y, z
    and rotation as quaternion
    as a csv with headers
    script in desktopsetup scene attached to main camera

--- Assets/Scripts/Interaction_System/LocationTracker.cs
+++ Assets/Scripts/Interaction_System/LocationTracker.cs
@@ -5,53 +5,72 @@ using System.IO;
  
 public class LocationTracker : MonoBehaviour
 {
-    [System.Serializable]
-    private struct MotionFrame
-    {
-      public int frameIndex;
-      public float timeStamp;
-      public Vector3 position;
-	  public Quaternion orientation;
-}
- 
-public GameObject vrcam;
- 
-private StreamWriter writer;
-private bool firstFrameWritten;
- 
-// Start is called before the first frame update
-void Start()
-{
-      string path = Application.persistentDataPath + "/" + "motion_" + System.DateTime.Now.ToString("s") + ".csv";
-      writer = new StreamWriter(path, false);
-      writer.AutoFlush = false;
-      writer.WriteLine("{");
-      writer.WriteLine("\"frames\":[");
-}
- 
-// Update is called once per frame
-void LateUpdate()
-{
-      MotionFrame frame;
-      frame.frameIndex = Time.frameCount;
-      frame.timeStamp = Time.time;
-      frame.position = vrcam.transform.position;
-      frame.orientation = vrcam.transform.rotation;
- 
-      string csvFrame = frame
+    string filename = "";
 
-      if(firstFrameWritten)
-      {
-        writer.WriteLine(",");
-      }
-      writer.Write(csvFrame);
-      writer.FlushAsync();
-      firstFrameWritten = true;
+    public GameObject vrcam;
+    public StreamWriter writer;
+
+    Vector3 position;
+    Quaternion orientation;
+
+    private int currentFrame = 0;
+    [SerializeField]
+    private int trackerResolution = 30;
+
+    public bool createdTrackData = false;
+
+    // Start is called before the first frame update
+    void Start()
+{
+      filename = Application.dataPath + "/" + "test.csv";
+
+
+}
+
+
+
+// Update is called once per frame
+void Update()
+{
+
+
+        if (currentFrame > trackerResolution) 
+        {
+            WriteData();
+            currentFrame = 0;
+        }
+
+        currentFrame += 1;
+
 }
  
-void OnApplicationQuit() {
-      writer.WriteLine("]");
-      writer.WriteLine("}");
-      writer.Close();
-}
+public void WriteData() 
+    {
+        if (!createdTrackData)
+        {
+            using (TextWriter tw = File.CreateText(filename))
+            {
+                tw.WriteLine("PosX,"+"PosY,"+"PosZ," + "Q1,"+"Q2,"+"Q3,"+"Q4,");
+            }
+            createdTrackData = true;
+        }
+        else
+        {
+            //get vector3 of XRrig or camera position (x,y,z) and quaternion of rotation
+            position = vrcam.transform.position;
+            orientation = vrcam.transform.rotation;
+
+            //remove () for the csv
+            string pos = position.ToString().Replace("(", "").Replace(")", "");
+            string ori = orientation.ToString().Replace("(", "").Replace(")", ""); 
+
+            using (TextWriter tw = File.AppendText(filename))
+            {
+                tw.WriteLine(pos + "," + ori + ",");
+                tw.Close();
+            }
+
+
+        }
+     }
 }
